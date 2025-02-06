fe94421205a99322a2bbba62def91b04a5985eb7
BakQ <rbxo9890@gmail.com>
2025-02-06 13:31:56 +0900

    #7.5 AnimationController

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -7,24 +7,65 @@ import 'package:visibility_detector/visibility_detector.dart';
 class VideoPost extends StatefulWidget {
   final Function onVideoFinished;
   final int index;
-  const VideoPost(
-      {super.key, required this.onVideoFinished, required this.index});
+
+  const VideoPost({
+    super.key,
+    required this.onVideoFinished,
+    required this.index,
+  });
 
   @override
   State<VideoPost> createState() => _VideoPostState();
 }
 
-class _VideoPostState extends State<VideoPost> {
-// VideoPlayerController 사용 시, 초기화 작업을 해주어야 영상을 불러올 수 있음
+class _VideoPostState extends State<VideoPost>
+    with SingleTickerProviderStateMixin {
+  // VideoPlayerController 사용 시, 초기화 작업을 해주어야 영상을 불러올 수 있음
   final VideoPlayerController _videoPlayerController =
       VideoPlayerController.asset("assets/videos/video01.mp4");
 
+  late final AnimationController _animationController;
+
+  final Duration _animationDuration = const Duration(milliseconds: 200);
+  bool _isPaused = false;
+
   void _initVideoPlayer() async {
     await _videoPlayerController.initialize(); // 초기화
-    //_videoPlayerController.play(); // 자동 재생 X
+    // _videoPlayerController.play(); // 자동 재생 X
     setState(() {});
+
     // 영상이 끝났는지 확인하기 위한 Listener
     _videoPlayerController.addListener(_onVideoChange);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initVideoPlayer();
+
+    _animationController = AnimationController(
+      vsync: this,
+      lowerBound: 1.0, // 크기 지정
+      upperBound: 1.5, // 크기 지정
+      value: 1.5, // default 크기
+      duration: _animationDuration,
+    );
+
+    /**_animationController.reverse()
+     * 수행 시, 1.5 => 1.0 으로 값이 바뀌게 되는데,
+     * build() 는 1.5, 1.0 일 때만 재수행 되고 있음
+     * build() 가 값이 바뀌는 것을 알게 하려면? _animationController 에 이벤트 리스너 추가
+     * 모든 단계에서 build 메서드를 실행하기 위해 이벤트 리스너에 setState() 추가
+     */
+    _animationController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
   }
 
   void _onVideoChange() {
@@ -49,21 +90,14 @@ class _VideoPostState extends State<VideoPost> {
   void _onTogglePause() {
     if (_videoPlayerController.value.isPlaying) {
       _videoPlayerController.pause();
+      _animationController.reverse(); // lowerBound
     } else {
       _videoPlayerController.play();
+      _animationController.forward(); // upperBound
     }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _initVideoPlayer();
-  }
-
-  @override
-  void dispose() {
-    _videoPlayerController.dispose();
-    super.dispose();
+    setState(() {
+      _isPaused = !_isPaused;
+    });
   }
 
   @override
@@ -87,14 +121,23 @@ class _VideoPostState extends State<VideoPost> {
               onTap: _onTogglePause,
             ),
           ),
-          const Positioned.fill(
+          Positioned.fill(
             // 클릭 이벤트를 무시하도록 함 (위의 _onTogglePause 가 먹도록)
             child: IgnorePointer(
               child: Center(
-                child: FaIcon(
-                  FontAwesomeIcons.play,
-                  color: Colors.white,
-                  size: Sizes.size52,
+                // 사이즈 조절을 위해 AnimationController 사용
+                child: Transform.scale(
+                  scale: _animationController.value,
+                  // Animated Widget 을 사용
+                  child: AnimatedOpacity(
+                    opacity: _isPaused ? 1 : 0,
+                    duration: _animationDuration,
+                    child: const FaIcon(
+                      FontAwesomeIcons.play,
+                      color: Colors.white,
+                      size: Sizes.size52,
+                    ),
+                  ),
                 ),
               ),
             ),
