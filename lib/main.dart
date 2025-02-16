78c6182236c9c283b0bf159d7dcfcea05dfe1d6d
BakQ <>
2025-02-17 01:09:25 +0900

    #15.3 Google Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -30,35 +30,8 @@ class TikTokApp extends StatelessWidget {
 
       // ✅ 라이트 모드 테마 설정
       theme: ThemeData(
+        textTheme: GoogleFonts.itimTextTheme(),
         brightness: Brightness.light, // 밝은 테마
-        textTheme: TextTheme(
-          displayLarge: GoogleFonts.openSans(
-              fontSize: 95, fontWeight: FontWeight.w300, letterSpacing: -1.5),
-          displayMedium: GoogleFonts.openSans(
-              fontSize: 59, fontWeight: FontWeight.w300, letterSpacing: -0.5),
-          displaySmall:
-              GoogleFonts.openSans(fontSize: 48, fontWeight: FontWeight.w400),
-          headlineMedium: GoogleFonts.openSans(
-              fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-          headlineSmall:
-              GoogleFonts.openSans(fontSize: 24, fontWeight: FontWeight.w400),
-          titleLarge: GoogleFonts.openSans(
-              fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
-          titleMedium: GoogleFonts.openSans(
-              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
-          titleSmall: GoogleFonts.openSans(
-              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
-          bodyLarge: GoogleFonts.roboto(
-              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
-          bodyMedium: GoogleFonts.roboto(
-              fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-          labelLarge: GoogleFonts.roboto(
-              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
-          bodySmall: GoogleFonts.roboto(
-              fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
-          labelSmall: GoogleFonts.roboto(
-              fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
-        ),
 
         scaffoldBackgroundColor: Colors.white, // 배경색 흰색
         primaryColor: const Color(0xFFE9435A), // 기본 색상 (빨간색)
@@ -85,6 +58,9 @@ class TikTokApp extends StatelessWidget {
 
       // ✅ 다크 모드 테마 설정
       darkTheme: ThemeData(
+        textTheme: GoogleFonts.itimTextTheme(
+          ThemeData(brightness: Brightness.dark).textTheme,
+        ),
         brightness: Brightness.dark, // 어두운 테마
         scaffoldBackgroundColor: Colors.black, // 배경색 검정
         bottomAppBarTheme: BottomAppBarTheme(
